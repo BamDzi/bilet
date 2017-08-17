7ece8f1eb055f11079df99b929c592fe5efbf9a4
BamDzi <pzajac82@gmail.com>
2017-08-17 06:11:26 +0200

    greet commit

---
+++ src/main/java/pl/testowy/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package pl.testowy.controller;
+
+import java.util.Optional;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetingController {
+
+	 @GetMapping({"/{person}"})
+	    public String greetPerson(@PathVariable(name="person", required=false) Optional<String> maybePerson) {
+	        String person = maybePerson.filter(StringUtils::isNotBlank).orElse("unknown person");
+	        return String.format("Hello %s!", person);
+	    }
+}
